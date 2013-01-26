37c270f300bf881f92259efcf23c7d2c7342c0bf
cmu.bruce@gmail.com <cmu.bruce@gmail.com@bcf419de-4a8b-6121-3915-16c4c4d6f229>
2013-01-26 22:26:53 +0000

    player.cs added

---
+++ trunk/HeartGame/HeartGame/HeartGame/Components/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace HeartGame
+{
+    public class Player : Person
+    {
+        public Player(string name, Vector3 position,
+                      ComponentManager componentManager,
+                      ContentManager content,
+                      GraphicsDevice graphics,
+                      string spritesheet) :
+            base(name, position, componentManager, content, graphics, spritesheet)
+        {
+        }
+    }
+}
