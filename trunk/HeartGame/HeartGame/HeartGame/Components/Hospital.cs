499c46d0f24099768f681bcf9e27b73797552623
versiontwopointzero <versiontwopointzero@bcf419de-4a8b-6121-3915-16c4c4d6f229>
2013-01-27 03:39:51 +0000

    added thing

---
+++ trunk/HeartGame/HeartGame/HeartGame/Components/Hospital.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace HeartGame
+{
+    public class Hospital
+    {
+        public Player Owner { get; set; }
+        public LocatableComponent Component { get; set; }
+
+        public Hospital(Player owner,  Vector3 position, Vector3 size, ComponentManager componentManager,
+                                                  ContentManager content,
+                                                  GraphicsDevice graphics,
+                                                  string name)
+        {
+            Owner = owner;
+            Component = EntityFactory.GenerateBuilding(position, size, componentManager, content, graphics, name);
+        }
+    }
+}
