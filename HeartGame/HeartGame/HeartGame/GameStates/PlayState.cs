19597e2fd9f8cbc2eeb0f781a295b775bd9ac6d4
versiontwopointzero <versiontwopointzero@bcf419de-4a8b-6121-3915-16c4c4d6f229>
2013-01-27 16:30:32 +0000

    colors are correct

--- HeartGame/HeartGame/HeartGame/GameStates/PlayState.cs
+++ HeartGame/HeartGame/HeartGame/GameStates/PlayState.cs
@@ -114,7 +114,7 @@ namespace HeartGame
 
             
             Hospital hospital1 = new Hospital(new Vector3(-15, 0, -15), new Vector3(4, 2, 3), ComponentManager, Game.Content, Game.GraphicsDevice, "hospital", Color.Red, new Point(2, 0));
-            Hospital hospital2 = new Hospital(new Vector3(15, 0, 15), new Vector3(4, 2, 3), ComponentManager, Game.Content, Game.GraphicsDevice, "hospital", Color.Green, new Point(1, 0));
+            Hospital hospital2 = new Hospital(new Vector3(15, 0, 15), new Vector3(4, 2, 3), ComponentManager, Game.Content, Game.GraphicsDevice, "hospital", Color.Blue, new Point(1, 0));
             hospitals.Add(hospital1);
             hospitals.Add(hospital2);
              
